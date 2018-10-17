5188deafc437ad61e23de75f6124ccfef8d2138b
admin <admin@stu7753b1>
2018-10-17 15:12:20 +0800

    second

--- src/main/java/com/bwf/controller/TestController.java
+++ src/main/java/com/bwf/controller/TestController.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("test")
 public class TestController {
 
-	@RequestMapping("test")
+	@RequestMapping("test1")
 	public void test(){
 		System.out.println(  "test controller" );
 	}
